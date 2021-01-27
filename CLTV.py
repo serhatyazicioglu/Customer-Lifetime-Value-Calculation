a0b3f26efc8368ad1b03673ed0a012ae00ed6ddb
Serhat Yazıcıoğlu <75208775+serhatyazicioglu@users.noreply.github.com>
2021-01-27 17:19:02 +0300

    Update CLTV.py

--- CLTV.py
+++ CLTV.py
@@ -7,19 +7,14 @@ pd.set_option('display.float_format', lambda x: '%.5f' % x)
 df_ = pd.read_excel("online_retail_II.xlsx", sheet_name="Year 2010-2011")
 df = df_.copy()
 
-# veri setinden iptal edilen ürünleri ayırdık
 df[~df["Invoice"].str.contains("C", na=False)]
 
-# Quantity içerisindeki negatif değerleri ayırdık
 df = df[df["Quantity"] > 0]
 
-# veri setindeki boş değerleri eledik
 df.dropna(inplace=True)
 
-# bir üründen kazanılan toplam ücret için yeni bir değişken atadık.
 df["TotalPrice"] = df["Quantity"] * df["Price"]
 
-# cltv değerleri için gerekli değerleri gruplama işlemi ile getirdik.
 df_cltv = df.groupby('Customer ID').agg({'Invoice': lambda x: len(x),
                                          'Quantity': lambda x: x.sum(),
                                          'TotalPrice': lambda x: x.sum()})
@@ -29,10 +24,10 @@ df_cltv.columns = ['total_transaction', 'total_unit', 'total_price']
 #  Customer_Value = Average_Order_Value * Purchase_Frequency
 df_cltv["average_order_value"] = df_cltv["total_price"] / df_cltv["total_transaction"]
 
-df_cltv.shape[0]  # toplam müsteri sayısı
+df_cltv.shape[0]  # total customer number
 df_cltv["purchase_frequency"] = df_cltv['total_transaction'] / df_cltv.shape[0]
 
-# customer value değerini bulduk.
+# customer value 
 df_cltv["customer_value"] = df_cltv["average_order_value"] * df_cltv["purchase_frequency"]
 
 # Churn_Rate = 1 - Repeat_Rate
@@ -46,7 +41,7 @@ df_cltv["profit_margin"] = df_cltv["total_price"] * 0.05
 # CLTV = (Customer_Value / Churn_Rate) x Profit_margin.
 df_cltv["CLTV"] = (df_cltv["customer_value"] / churn_rate) * df_cltv["profit_margin"]
 
-# segmentlere ayırdık
+# segmentation
 df_cltv["Segment"] = pd.qcut(df_cltv["CLTV"], 5, labels=["E", "D", "C", "B", "A"])
 
 df_cltv[["total_price", "CLTV", "Segment"]].sort_values(by="Segment", ascending=False).head()
